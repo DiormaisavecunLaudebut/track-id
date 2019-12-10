174208460043339aa67f1543585b3c40095a87b0
Coralie Delpha <coralie.delpha@gmail.com>
2019-12-10 15:20:00 +0100

    added uniqueness validation to email and username in the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :email, presence: true
-  validates :username, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
 end
