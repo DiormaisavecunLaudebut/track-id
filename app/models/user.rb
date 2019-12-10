689a320734fff188b74afa6a805b3f3562118173
Coralie Delpha <coralie.delpha@gmail.com>
2019-12-10 15:16:00 +0100

    added email and username validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,14 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   mount_uploader :photo, PhotoUploader
+
   has_many :posts, dependent: :destroy
   has_many :messages, dependent: :destroy
   has_many :upvotes
   has_many :views, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true
+  validates :username, presence: true
 end
