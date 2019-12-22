cae7ceee75c30ba1c79a37db1ae25b51d2fc60e8
Lior Levy <lior.pmtraining@gmail.com>
2019-12-22 15:57:00 +0100

    ça seed NRV

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,58 +7,46 @@ ending = ["?", ".", "!", "!!", "!!!", "?", "", "", ""]
 url = "https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_960_720.png"
 # replace url by this line if you a random avatar pic, and not the same again and again
 # url = "https://i.pravatar.cc/300"
-test = false
 
-if test == false
-  clear_database
-  seed_database
-else
-  RSpotify.authenticate(ENV["SPOTIFY_CLIENT"], ENV["SPOTIFY_SECRET"])
-  tracks = RSpotify::Base.search('cory hery trade it all', 'track')
-  print tracks
-end
+User.destroy_all
+Post.destroy_all
+Message.destroy_all
+Upvote.destroy_all
+View.destroy_all
 
-def clear_database
-  User.destroy_all
-  Post.destroy_all
-  Message.destroy_all
-  Upvote.destroy_all
-  View.destroy_all
-end
 
-def seed_database
-  100.times do |i|
-    user = User.new(
-      email: Faker::Internet.email + i.to_s,
-      username: Faker::Name.first_name + i.to_s,
-      password: "atlantis"
+100.times do |i|
+  user = User.new(
+    email: Faker::Internet.email + i.to_s,
+    username: Faker::Name.first_name + i.to_s,
+    password: "atlantis"
+  )
+  # user.remote_photo_url = url
+  user.save!
+  post = Post.create(
+    title: "Please help me find this track !",
+    user: user
+  )
+  (10..22).to_a.sample.times do
+    View.create(
+      user: user,
+      post: Post.all.sample
     )
-    # user.remote_photo_url = url
-    user.save!
-    post = Post.create(
-      title: "Please help me find this track !",
+  end
+  (3..8).to_a.sample.times do
+    message = Message.create(
+      body: "#{form.sample} /#{Faker::Music::RockBand.name}/ #{ending.sample}",
+      post: Post.all.sample,
       user: user
     )
-    (10..22).to_a.sample.times do
-      View.create(
-        user: user,
-        post: Post.all.sample
-      )
-    end
-    (3..8).to_a.sample.times do
-      message = Message.create(
-        body: "#{form.sample} /#{Faker::Music::RockBand.name}/ #{ending.sample}",
-        post: Post.all.sample,
-        user: user
-      )
-    end
-    (0..30).to_a.sample.times do
-      upvote = Upvote.create(
-        user: user,
-        message: Message.all.sample
-      )
-    end
+  end
+  (0..30).to_a.sample.times do
+    upvote = Upvote.create(
+      user: user,
+      message: Message.all.sample
+    )
   end
 end
+
 
 
